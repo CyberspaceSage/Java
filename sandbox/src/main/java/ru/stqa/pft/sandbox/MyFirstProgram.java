dce9cd8521b6d754aeba1f2118149ac41078a2c7
CyberspaceSage <135464216+CyberspaceSage@users.noreply.github.com>
2023-06-09 01:18:09 +0300

    Создан класс Point и функция вычисления квадратного корня

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -8,22 +8,35 @@ public class MyFirstProgram {
 		hello("user");
 		hello("Alex");
 
-		double l = 5;
-		System.out.println("Площаль квадрата со стороной "+ l + " = " + area(l));
 
-		double a = 4;
-		double b = 6;
-		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area  (a,b));
+		Point P = new Point ();
+		P.p1 = 4;
+		P.p2 = 7;
+		P.p3 = 8;
+		P.p4 = 9;
+		System.out.println("Points distance =" + distance(P));
+
+
+		Square s = new Square(5);
+		System.out.println("Площаль квадрата со стороной "+ s.l + " = " + area(s));
+
+		Rectangle r = new Rectangle(4,6);
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area  (r));
 		}
 
 		public static void hello(String somebody) {
 			System.out.println("Hello," + somebody + "!");
 		}
 
-		public static double area (double len){
-		return len * len;
+		public static double area (Square s){
+		return s.l * s.l;
 		}
-		public static double area (double a, double b){
-		return a * b;
+		public static double area (Rectangle r){
+		return r.a * r.b;
 		}
-	}
+		public static double distance  (Point P){
+		return Math.sqrt((P.p3-P.p1)*(P.p3-P.p1)+(P.p4-P.p2)*(P.p4-P.p2));
+				}
+       }
+
+
---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -0,0 +1,12 @@
+package ru.stqa.pft.sandbox;
+
+public class Point {
+        public double p1;
+        public double p2;
+        public double p3;
+        public double p4;
+
+
+        }
+
+
