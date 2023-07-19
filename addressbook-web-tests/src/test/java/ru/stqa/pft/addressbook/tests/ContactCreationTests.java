54957e5e6e99bbe9737cdb2051666916f2294715
CyberspaceSage <135464216+CyberspaceSage@users.noreply.github.com>
2023-07-20 01:01:57 +0300

    Задание №10: Реализованы проверки в fluent-стиле

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactCreationTests.java
@@ -1,11 +1,11 @@
 package ru.stqa.pft.addressbook.tests;
-
-import org.testng.Assert;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.Contacts;
 import ru.stqa.pft.addressbook.model.GroupData;
-import java.util.Set;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 public class ContactCreationTests extends TestBase {
 
   @BeforeMethod
@@ -20,18 +20,16 @@ public class ContactCreationTests extends TestBase {
   public void testContactCreation() throws Exception {
 
     app.contact().gotoHomePage();
-    Set<ContactData> before = app.contact().all();
+    Contacts before = app.contact().all();
     ContactData contact = new ContactData().withFirstname("Ivan").withMiddlename("Ivanovich").withLastname("Ivanov").withCompany("TCC")
             .withAddress("Russia, 19034, Moscow, Petrovka, 10").withHomePhone("7 (495) 325-24-15").withMobilePhone("+7 916-000-00-00")
             .withEmail("test@test.com").withGroup("test3");
     app.goTo().addContactPage();
     app.contact().create(contact);
     app.contact().gotoHomePage();
-    Set <ContactData> after = app.contact().all();
-    Assert.assertEquals(after.size(),before.size() + 1);
-
-    contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt());
-    before.add(contact);
-    Assert.assertEquals(before, after);
+    Contacts after = app.contact().all();
+    assertThat(after.size(),equalTo(before.size() + 1));
+    assertThat(after, equalTo(before.withAdded(
+            contact.withId(after.stream().mapToInt((g) -> g.getId()).max().getAsInt()))));
   }
 }
